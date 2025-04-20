e2a236a8884675a85aa7266016a21f066fd57e87
VeriSafeAgent <VeriSafeAgent@gmail.com>
2025-04-21 06:54:33 +0900

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -7,7 +7,7 @@ import os
 from utils_.utils_ import log
 
 
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = ""
 
 #python simulator.py --mobile_device emulator --agent m3a --verifier vsa --app com.google.android.deskclock --instruction "make alarm am 3:00 and set the timer to 30sec." --save_path ./history/clock\
 #python simulator.py --mobile_device emulator --agent m3a --verifier vsa --app com.google.android.deskclock --instruction "Open the Clock app and create three new alarms with the following conditions: The first alarm should be set for 6:30 AM on weekdays (Monday to Friday) and labeled "Morning Workout". The second alarm should be set for 8:00 AM on weekends (Saturday and Sunday) and labeled "Relaxed Morning". The third alarm should be set for 10:00 PM every night and labeled "Wind Down" with a vibration-only mode. Once all alarms are created, go to the alarm list, and turn them all on." --save_path ./history/clock
